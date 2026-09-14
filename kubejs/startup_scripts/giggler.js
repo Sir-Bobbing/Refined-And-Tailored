7bcd5a133f160315c817cf4f7d3ae1408ee8b05d
ShadowzOfTheDark <57125303+ShadowzOfTheDark@users.noreply.github.com>
2026-09-14 18:17:23 -0500

    giggler fix

--- kubejs/startup_scripts/giggler.js
+++ kubejs/startup_scripts/giggler.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('item', event => {
 
-    const enabled = true
+    const enabled = false
     const range = 8
 
     function getRandomNearbyCreature( level, pos, user ) {
@@ -25,11 +25,13 @@ StartupEvents.registry('item', event => {
         lib.runServerCommand(level,`particle minecraft:end_rod ${entity.x} ${centerY} ${entity.z} 0.25 0.25 0.25 0.25 50 force`)
 
         if (entity.isPlayer()) {
-            const keep = lib.runServerCommand(level,'gamerule keepInventory') == 'Gamerule keepInventory is currently set to: true'
-            if (!keep) {lib.runServerCommand(level,'gamerule keepInventory true')}
+            let gameRules = level.server.getGameRules()
+            const keep = gameRules.get('keepInventory').get()
+            console.log('Giggler keep',keep)
+            if (!keep) {gameRules.set('keepInventory', 'true')}
             entity.damage(10000, new DamageSource['(net.minecraft.core.Holder,net.minecraft.world.entity.Entity)']('kubejs:giggler', source))
             entity.setPos(entity.x, entity.y - 6, entity.z)
-            if (!keep) {lib.runServerCommand(level,'gamerule keepInventory false')}
+            if (!keep) {gameRules.set('keepInventory', 'false')}
         }
         else {
             entity.setPos(entity.x, -1024, entity.z)
@@ -54,14 +56,14 @@ StartupEvents.registry('item', event => {
             return true
         })
         .finishUsing((itemstack, level, entity) => {
+            
             if (!level.isClientSide()) {
                 if (entity.isFakePlayer()) {return itemstack}
                 let nearbyEntity = getRandomNearbyCreature(level, entity.blockPosition())
                 if (nearbyEntity == null) {nearbyEntity = entity}
                 if (nearbyEntity != null) {
-                    popCreature( level, nearbyEntity, entity )
-                    // Long cooldown if you target yourself with the giggler
                     entity.addItemCooldown("kubejs:giggler", (nearbyEntity == entity ?  20 : 3) * 20)
+                    popCreature( level, nearbyEntity, entity )
                     itemstack.shrink(1)
                 } 
             }
